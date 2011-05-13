25d13b7ff8309bde920f02077c2fef666da7cbb2
Brennan Novak <brennan@reverseproductions.com>
2011-05-13 10:48:19 -0700

    added things

---
+++ config/install.php
@@ -0,0 +1,19 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:			Social Igniter : Messages : Install
+* Author: 		Brennan Novak
+* 		  		contact@social-igniter.com
+*         		@brennannovak
+*          
+* Created: 		Brennan Novak
+*
+* Project:		http://social-igniter.com/
+* Source: 		http://github.com/socialigniter/messages
+*
+* Description: 	Install values for Messages App for Social Igniter 
+*/
+/* Settings */
+$config['messages_settings']['enabled']				= 'TRUE';
+$config['messages_settings']['display_style'] 		= 'linear';
+$config['messages_settings']['date_style']			= 'DIGITS';
+$config['messages_settings']['notifications_email']	= 'TRUE';
